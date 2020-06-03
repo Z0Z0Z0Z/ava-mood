5aff6ce6a6be702652e13429f4973abf202782dc
Z0Z0Z0Z <>
2020-06-03 19:20:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import telethon as th
 app_id = os.getenv('app_id')
 app_hash = os.getenv('app_hash')
 session_name = "login"
+print(app_hash)
 client = th.TelegramClient(session_name, app_id, app_hash)
 client.send_code_request('+380931684539')
 client.start()
