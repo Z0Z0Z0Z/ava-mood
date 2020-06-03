c3e0557e2d258431725c9b64ca8b5075a222ba6b
Z0Z0Z0Z <>
2020-06-03 19:48:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import os
-import telethon as th
 
-app_id = "1150077"
-app_hash = "162842e9d791c876a61d1791047d7971"
-session_name = "login"
-print(app_hash)
-client = th.TelegramClient(session_name, api_id=app_id, api_hash=app_hash)
-client.start()
-for i in range(1, 100):
-	client.send_message('@mi_xxx', 'Hello! Message num: {} '.format(i))
