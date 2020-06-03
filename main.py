c79047f24de9c1952bba0b00cced320395b9a143
Z0Z0Z0Z <>
2020-06-03 19:08:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import telethon as th
 
 app_id = os.getenv('app_id')
-app_hash = str(os.getenv('app_hash'))
-session_name = "mixxx"
+app_hash = os.getenv('app_hash')
+session_name = "login.session"
 client = th.TelegramClient(session_name, app_id, app_hash)
 client.start()
 
